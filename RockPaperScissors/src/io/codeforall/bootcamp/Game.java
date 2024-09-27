be8404ad9507372aa7dc1bdd8316f941435ddf0f
axel <ssaxel03@gmail.com>
2024-09-27 16:49:13 +0100

    updated rock paper scissors to encapsulate game logic in game class

--- RockPaperScissors/src/io/codeforall/bootcamp/Game.java
+++ RockPaperScissors/src/io/codeforall/bootcamp/Game.java
@@ -4,25 +4,38 @@ public class Game {
 
     public final static int MAX_ROUNDS = 3;
 
-    public static void play(Player player1, Player player2) {
+    public static void play() {
+
+        Player player1 = new Player("Pedro Morais");
+        Player player2 = new Player("Axel Soares");
+
+        System.out.println("Start of game");
+
+        int limit = (Game.MAX_ROUNDS / 2) + 1;
+
+        System.out.println(limit+"");
 
         // compare moves
+        while(!(player1.getWins() == limit || player2.getWins() == limit)) {
 
-        System.out.println(player1.getName() + " played " + player1.getCurrentMove().getMoveName());
-        System.out.println(player2.getName() + " played " + player2.getCurrentMove().getMoveName());
+            System.out.println(player1.getName() + ": " + player1.getWins());
+            System.out.println(player2.getName() + ": " + player2.getWins());
 
-        if(Move.getWinner(player1.getCurrentMove(), player2.getCurrentMove()) == 1) {
-            System.out.println(player1.getName() + " wins");
-            player1.incWins();
+            player1.setCurrentMove(Move.values()[RandomGenerator.generateInt()]);
+            player2.setCurrentMove(Move.values()[RandomGenerator.generateInt()]);
+
+            System.out.println(player1.getName() + " played " + player1.getCurrentMove().getMoveName());
+            System.out.println(player2.getName() + " played " + player2.getCurrentMove().getMoveName());
+
+            comparisons(player1, player2);
+
+        }
+
+        if(player1.getWins() > player2.getWins()){
+            System.out.println(player1.getName() + " is the game winner with " + player1.getWins() + " wins");
             return;
         }
-        if(Move.getWinner(player1.getCurrentMove(), player2.getCurrentMove()) == 2) {
-            System.out.println(player2.getName() + " wins");
-            player2.incWins();
-            return;
-        }
-
-        System.out.println("It's a draw");
+        System.out.println(player2.getName() + " is the game winner with " + player2.getWins() + " wins");
 
 
         /*
@@ -66,6 +79,23 @@ public class Game {
         }
 
         */
+
+    }
+
+    public static void comparisons(Player player1, Player player2) {
+
+        if (Move.getWinner(player1.getCurrentMove(), player2.getCurrentMove()) == 1) {
+            System.out.println(player1.getName() + " wins");
+            player1.incWins();
+            return;
+        }
+        if (Move.getWinner(player1.getCurrentMove(), player2.getCurrentMove()) == 2) {
+            System.out.println(player2.getName() + " wins");
+            player2.incWins();
+            return;
+        }
+
+        System.out.println("It's a draw");
 
     }
 
