eef015d0daee80c58cb8a24a3416d8abc665e858
axel <ssaxel03@gmail.com>
2024-10-08 18:00:40 +0100

    add: Added Simplegraphicsv2 library documentation and executable

---
+++ simplegraphics-docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.academiadecodigo.simplegraphics.graphics"},{"l":"org.academiadecodigo.simplegraphics.keyboard"},{"l":"org.academiadecodigo.simplegraphics.mouse"},{"l":"org.academiadecodigo.simplegraphics.pictures"}];updateSearchResults();
