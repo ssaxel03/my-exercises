930135258ac5fdb1d3350691c82fa81b586bf0ad
axel <ssaxel03@gmail.com>
2024-10-23 14:34:41 +0100

    Added some changes

--- UPDUpperCaseServer/src/io/codeforall/fanstatics/Main.java
+++ UPDUpperCaseServer/src/io/codeforall/fanstatics/Main.java
@@ -10,19 +10,28 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        boolean keepGoing = true;
+
         int localPort = 8080;
 
         DatagramSocket socket = new DatagramSocket(localPort);
 
-        while(true) {
+        while(keepGoing) {
             byte[] recvBuffer = new byte[1024];
 
             DatagramPacket receivePacket = new DatagramPacket(recvBuffer, recvBuffer.length);
             socket.receive(receivePacket);
 
             String uppercase = new String(recvBuffer, 0, receivePacket.getLength()).toUpperCase();
+            uppercase = uppercase.replaceAll("\n", "");
+            System.out.println(uppercase);
 
-            byte[] sendBuffer = uppercase.getBytes();
+            if(uppercase.equals("Q")) {
+                socket.close();
+                System.exit(0);
+            }
+
+            byte[] sendBuffer = (uppercase + "\n").getBytes();
 
             DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, receivePacket.getAddress(), receivePacket.getPort());
             socket.send(sendPacket);
