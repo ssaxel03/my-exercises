4a923eaa30f52476117ee2cbe1d9ed84aa5ba4a1
axel <ssaxel03@gmail.com>
2024-11-04 14:32:22 +0000

    add: added RemoveGarbageFunctional program

---
+++ RemoveGarbageFunctional/src/io/codeforall/fanstatics/Main.java
@@ -0,0 +1,21 @@
+package io.codeforall.fanstatics;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String message = "I'll send an SOS to the garbage world, " +
+                "I hope that someone garbage gets my message in a garbage bottle.";
+
+        System.out.println(Arrays.stream(message.split(" "))
+                .map(String::toUpperCase)
+                .filter(word -> !Objects.equals(word, "GARBAGE"))
+                .reduce("", (acc, elem) -> acc + " " + elem));
+
+    }
+
+}
